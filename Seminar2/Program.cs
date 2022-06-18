286f1be6e5f8195bb658abf9264c79d33f2de2e3
Olya <Zlobinaolya87@gmail.com>
2022-06-18 20:14:37 +0300

    name changed

---
+++ Seminar2/Program.cs
@@ -0,0 +1,99 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write(" Input three digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+void secondNumber()
+{   int num3 = num / 10 % 10;
+    Console.WriteLine(num + " -> " + num3);
+}
+secondNumber();
+
+
+// 2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine(" Input number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+void ThirdNum()
+{
+    if (N > 100 && N < 999)
+    {
+        int third = N % 10;
+        Console.WriteLine(N + " -> " + third);
+    }
+    else
+    {
+        if (N > 1 && N < 99)
+            Console.WriteLine("No third digit");
+    }
+}
+ThirdNum();
+
+
+//3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine(" Input number: ");
+int day = Convert.ToInt32(Console.ReadLine());
+void Weekends(){
+    if (day < 1 || day > 7){
+        Console.WriteLine("Incorrect number! ");
+    }
+    else{
+        if(day == 1){
+            Console.WriteLine("Monday - not a weekend ");
+        }
+        if(day == 2){
+            Console.WriteLine("Tuesday - not a weekend ");
+        }
+        if(day == 3){
+            Console.WriteLine("Wednesday - not a weekend ");
+        }
+        if(day == 4){
+            Console.WriteLine("Thursday - not a weekend ");
+        }
+        if(day == 5){
+            Console.WriteLine("Friday - not a weekend ");
+        }
+        if(day == 6){
+            Console.WriteLine("Saturday - weekend ");
+        }
+        if(day == 7){
+            Console.WriteLine("Sunday - weekend ");
+        }      
+    }
+}
+Weekends();
+
+// 1. Доп. задача. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine(" Input number: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+void Multiple(){
+    if (Num / 7 == 0 && Num / 23 == 0)
+    {
+        Console.WriteLine(" The number is a multiple of 7 and 23 ");
+    }
+    else
+    {
+        Console.WriteLine(" The number is not a multiple of 7 and 23 ");
+    }
+}
+Multiple();
+
+
+// 2. Доп. задача. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+void SquareNum(){
+    int result = num1 * num1;
+    if(num2 == result){
+        Console.WriteLine("The second number is the square of the first number ");
+    }
+    else{
+        Console.WriteLine("The second number is not the square of the first number");
+    }
+}
+SquareNum();
+
